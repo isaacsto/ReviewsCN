1b0ddaf92574a66197325ed1413dfe971ab78b7f
Isaac Stofko <isaacstofko@gmail.com>
2023-10-27 13:07:47 -0400

    changed fetch

--- client/script.js
+++ client/script.js
@@ -1,22 +1,22 @@
 document.getElementById('searchForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    const location = document.getElementById('locationInput').value;
-    const keyword = document.getElementById('keywordInput').value;
+  e.preventDefault();
+  const location = document.getElementById('locationInput').value;
+  const keyword = document.getElementById('keywordInput').value;
 
-    // Send the data to the server
-    fetch('/api/search', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ location, keyword }),
+  // Send the data to the server
+  fetch('http://localhost:3000/api/search', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ location, keyword }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      
+      document.getElementById('result').textContent = JSON.stringify(data, null, 2);
     })
-      .then(response => response.json())
-      .then(data => {
-        
-        document.getElementById('result').textContent = JSON.stringify(data, null, 2);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  });
+    .catch(error => {
+      console.error('Error:', error);
+    });
+});
