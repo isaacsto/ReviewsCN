a10477ac0db50ea3f6bc75aa12f7f182a0b4c637
Isaac Stofko <isaacstofko@gmail.com>
2023-11-03 10:53:19 -0400

    refactored fetch

--- client/script.js
+++ client/script.js
@@ -3,14 +3,13 @@ document.getElementById('searchForm').addEventListener('submit', function (e) {
   const location = document.getElementById('locationInput').value;
   const keyword = document.getElementById('keywordInput').value;
 
-
-  // Send the data to the server
+  // Send the data to the first API endpoint
   fetch('http://localhost:3000/api/search', {
-    method: 'POST',
+    method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ location, keyword }),
+    // You can't send a body in a GET request; remove this line
   })
     .then(response => response.json())
     .then(data => {
@@ -20,4 +19,24 @@ document.getElementById('searchForm').addEventListener('submit', function (e) {
     .catch(error => {
       console.error('Error:', error);
     });
+
+  // Send the data to the second API endpoint
+  fetch('http://localhost:3000/api/search/google_maps_reviews', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+   
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      
+      document.getElementById('result2').textContent = JSON.stringify(data, null, 2);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 });
+
+  
