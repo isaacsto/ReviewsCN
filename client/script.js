2c28fc258441193f8cfaf841dd76c825c60a3f66
Isaac Stofko <isaacstofko@gmail.com>
2023-11-06 14:47:45 -0500

    client.js

---
+++ client/script.js
@@ -0,0 +1,32 @@
+document.getElementById('searchForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  const location = document.getElementById('locationInput').value;
+  const keyword = document.getElementById('keywordInput').value;
+
+  // Send the data to the first API endpoint
+  fetch(`http://localhost:3000/api/search?location=${location}&keyword=${keyword}`, {
+    method: 'GET',
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+
+
+  // Send the data to the second API endpoint
+  fetch(`http://localhost:3000/api/search/google_maps_reviews`, {
+    method: 'GET',
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+});
+
+
