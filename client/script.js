95dbfa652ce5f1d2cfafed17d23e855f91e534f9
Isaac Stofko <isaacstofko@gmail.com>
2023-11-06 15:27:18 -0500

    server refactor

--- client/script.js
+++ client/script.js
@@ -1,23 +1,33 @@
-document.getElementById('searchForm').addEventListener('submit', function (e) {
+/*document.getElementById('searchForm').addEventListener('submit', function (e) {
   e.preventDefault();
   const location = document.getElementById('locationInput').value;
   const keyword = document.getElementById('keywordInput').value;
 
+
+
   // Send the data to the first API endpoint
-  fetch(`http://localhost:3000/api/search?location=${location}&keyword=${keyword}`, {
+  fetch('/api/search', {
     method: 'GET',
   })
-    .then(response => response.json())
+    .then(response => response.json()
     .then(data => {
-      console.log(data);
+      const dataId = data.id; 
+    })
+    )
+
+    fetch(`/api/google_maps_reviews?dataId=${dataId}`)
+    .then(response => response.json())
+    .then(reviewsData => {
+      console.log(reviewsData.reviews_results);
     })
     .catch(error => {
       console.error('Error:', error);
     });
-
-
-  // Send the data to the second API endpoint
-  fetch(`http://localhost:3000/api/search/google_maps_reviews`, {
+    
+  });  
+  /*
+    // Send the data to the second API endpoint
+  fetch( `/api/google-maps-reviews`, {
     method: 'GET',
   })
     .then(response => response.json())
@@ -29,4 +39,4 @@ document.getElementById('searchForm').addEventListener('submit', function (e) {
     });
 });
 
-
+*/
--- server/server.js
+++ server/server.js
@@ -2,6 +2,89 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
+const { getJson } = require("serpapi"); // Import getJson once
+
+// Enable CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// parse JSON
+app.use(express.json());
+
+// Path to client folder
+app.use(express.static(path.join(__dirname, '../client')));
+
+// Path to HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+// Path to env/api key
+require('dotenv').config();
+const API_key = process.env.API_key;
+
+// Call general search to get data id
+app.get('/api/search', (req, res) => {
+  try {
+    getJson({
+      api_key: API_key,
+      engine: "google",
+      q: req.query.keyword,
+      location: req.query.location,
+      google_domain: "google.com",
+      gl: "us",
+      hl: "en"
+    }, (json) => {
+      console.log(json["reviews_results"]);
+      const dataId = json["id"];
+
+      // define new route for fetching reviews
+      app.get(`/api/search/google_maps_reviews`, (req, res) => {
+        try {
+          getJson({
+            api_key: API_key,
+            engine: "google_maps_reviews",
+            data_id: dataId,
+            hl: "en"
+          }, (reviewsJson) => {
+            console.log(reviewsJson["reviews_results"]);
+            res.json(reviewsJson);
+          }).catch(function (error) {
+            console.log(error);
+            res.status(500).json({ error: 'An error occurred while fetching reviews data.' });
+          });
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ error: 'An error occurred while processing the request.', details: error.message });
+        }
+      });
+
+      res.json(json);
+    }).catch(function (error) {
+      console.log(error);
+      res.status(500).json({ error: 'An error occurred while fetching data.' });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while processing the request.', details: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Backend server is running on port ${port}`);
+});
+
+
+
+
+/* const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
 
 // Enable CORS
 app.use((req, res, next) => {
@@ -39,6 +122,7 @@ app.get('/api/search', (req, res) => {
       gl: "us",
       hl: "en"
         
+
     }, (json) => {
       console.log(json["reviews_results"]);
       //store data id in const 
@@ -97,4 +181,4 @@ getJson({
 app.listen(port, () => {
   console.log(`Backend server is running on port ${port}`);
 });
-
+*/
